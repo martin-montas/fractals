afb316540fe04921148de50406cf2666822daef0
martin-montas <martinmontas1@gmail.com>
2026-06-25 21:53:27 -0400

    COMMIT: Finishing the math utils lib

--- koch-snowflake/main.cpp
+++ koch-snowflake/main.cpp
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h>
 #include <vector>
 
+using namespace std;
 // Initial triangle
 std::vector<SDL_Vertex> verts = {
     {
@@ -24,49 +25,46 @@ std::vector<SDL_Vertex> verts = {
 };
 
 void interate_vertex(SDL_Vertex v) {
-  // size_t count = 0;
+    // size_t count = 0;
 }
 
-int main(int argc, char **argv) {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window *window =
-      SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                       800, 600, SDL_WINDOW_SHOWN);
-  SDL_Renderer *renderer = SDL_CreateRenderer(
-      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+int main(int argc, char** argv) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window*   window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                            800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-  SDL_FPoint v = calc_mid_point(verts[0].position, verts[2].position);
+    SDL_FPoint v = calc_mid_point(verts[0].position, verts[2].position);
 
-  SDL_Vertex vert2 = {
-      v,
-      SDL_Color{255, 0, 0, 255},
-      SDL_FPoint{0},
-  };
+    SDL_Vertex vert2 = {
+        v,
+        SDL_Color{255, 0, 0, 255},
+        SDL_FPoint{0},
+    };
 
-  // verts.push_back(vert2);
+    // verts.push_back(vert2);
 
-  bool running = true;
-  while (running) {
-    SDL_Event ev;
-    while (SDL_PollEvent(&ev)) {
-      if ((SDL_QUIT == ev.type) ||
-          (SDL_KEYDOWN == ev.type &&
-           SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode)) {
-        running = false;
-        break;
-      }
+    bool running = true;
+    while (running) {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            if ((SDL_QUIT == ev.type) ||
+                (SDL_KEYDOWN == ev.type && SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode)) {
+                running = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderGeometry(renderer, nullptr, verts.data(), verts.size(), nullptr, 0);
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderGeometry(renderer, nullptr, verts.data(), verts.size(), nullptr,
-                       0);
-    SDL_RenderPresent(renderer);
-  }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
+    return 0;
 }
--- koch-snowflake/src/mathutils.hpp
+++ koch-snowflake/src/mathutils.hpp
@@ -2,26 +2,39 @@
 #define SRC_MATHUTILS_HPP_
 
 #include <SDL2/SDL.h>
-#include <cmath>
 
 class MathUtils {
-public:
-  inline SDL_FPoint calc_mid_point_vec(SDL_FPoint v1, SDL_FPoint v2) {
-    // C = (A + B) / 2.
-    SDL_FPoint v3 = {(v1.x + v2.x) / 2, (v1.y + v2.y) / 2};
-    return v3;
-  }
-  inline float calc_distance_vec(SDL_FPoint v1, SDL_FPoint v2) {
-    // d = √(x1 – x2)2 + (y1 – y2 )
-    return sqrt(std::pow(v1.x - v2.x, 2) + std::pow(v1.y - v2.y, 2));
-  }
-  inline float calc_height_vec(SDL_FPoint v1, SDL_FPoint v2) {
-    // h = (√3 / 2) * r
+  public:
+    /* @brief: calculate the mid point between 2 vectors.
+     * @param[in]: the first vector
+     * @param[in]: the second vector
+     * @returns:  a properly changed vector
+     */
+    SDL_FPoint calc_mid_point_vec(SDL_FPoint v1, SDL_FPoint v2);
 
-    // TODO: calculate r here
-    float r;
-    return sqrt(3) / 2 * r;
-  }
+    /* @brief: calculate the distance between 2 vectors.
+     * @param[in]: the first vector
+     * @param[in]: the second vector
+     * @returns:  float: distance between vectors
+     */
+    float calc_distance_vec(SDL_FPoint v1, SDL_FPoint v2);
+
+    /* @brief: calculate the height between 2 vectors using the
+     * Pythagorean theorem.
+     * @param[in]: the first vector
+     * @param[in]: the second vector
+     * @returns:  float: height between vectors
+     */
+    float calc_height_vec(SDL_FPoint v1, SDL_FPoint v2);
+
+    /* @brief: calculate the third 1/3 vectex for of another
+     * vectex as explain here:
+     * https://nostarch.com/download/PythonPlayground_SampleChapter1.pdf
+     * @param[in]: the first vector
+     * @param[in]: the second vector
+     * @returns:  (SDL_FPoint) vector
+     */
+    SDL_FPoint calc_p2_vec(SDL_FPoint v1, SDL_FPoint v2);
 };
 
 #endif // SRC_MATHUTILS_HPP_
