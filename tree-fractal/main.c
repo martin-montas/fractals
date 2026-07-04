88cc79b13368515256bb8c8253a7400c7d63bb8b
martin-montas <martinmontas1@gmail.com>
2026-07-03 23:48:14 -0400

    FINISH: Think I'm done with the tree fractal set

--- tree-fractal/main.c
+++ tree-fractal/main.c
@@ -4,19 +4,6 @@
 #define WIDTH  1000
 #define HEIGHT 1000
 
-Vector2 draw_rotate_red(Vector2 start, Vector2 end, float angle, float scale) {
-    Vector2 localDirection = Vector2Subtract(start, end);
-    float   distance       = Vector2Distance(end, start);
-    Vector2 scaledLocal    = Vector2Scale(localDirection, scale);
-
-    Vector2 rotatedScaledLocal = Vector2Rotate(scaledLocal, angle);
-
-    Vector2 finalStart = Vector2Add(end, rotatedScaledLocal);
-
-    DrawLineV(finalStart, end, RED);
-    return finalStart;
-}
-
 Vector2 draw_rotate_blue(Vector2 start, Vector2 end, float angle, float scale) {
     Vector2 localDirection     = Vector2Subtract(start, end);
     float   distance           = Vector2Distance(end, start);
@@ -27,20 +14,22 @@ Vector2 draw_rotate_blue(Vector2 start, Vector2 end, float angle, float scale) {
     DrawLineV(finalStart, end, BLUE);
     return finalStart;
 }
-float rotate_right = 55.0f * DEG2RAD;
-float rotate_left  = 305.0f * DEG2RAD;
-float shrink       = 0.70f;
 
-void render_line(int x1, int y1, int depth, float shrink) {
+float rotate_right = 45.0f * DEG2RAD;
+float rotate_left  = 315.0f * DEG2RAD;
+float shrink       = 0.70f;
+void  render_line(int x1, int y1, int x2, int y2, int depth, float shrink) {
     if (depth == 0) {
         return;
     }
     Vector2 line_end   = {x1, y1};
-    Vector2 line_start = {x1, y1 / 2};
-    Vector2 left       = draw_rotate_blue(line_start, line_end, rotate_left - 30.0f, shrink);
-    Vector2 right      = draw_rotate_red(line_start, line_end, rotate_right + 30.0f, shrink);
-    render_line(left.x, left.y, depth - 1, shrink * 0.5f);
-    render_line(right.x, right.y, depth - 1, shrink * 0.5f);
+    Vector2 line_start = {x2, y2};
+
+    Vector2 start_left  = draw_rotate_blue(line_start, line_end, -40.0f, shrink);
+    Vector2 start_right = draw_rotate_blue(line_start, line_end, +40.0f, shrink);
+
+    render_line(start_left.x, start_left.y, line_end.x, line_end.y, depth - 1, shrink * 0.7f);
+    render_line(start_right.x, start_right.y, line_end.x, line_end.y, depth - 1, shrink * 0.7f);
 }
 
 int main() {
@@ -52,7 +41,7 @@ int main() {
         ClearBackground(BLACK);
 
         DrawLine(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT, BLUE);
-        render_line(WIDTH / 2, HEIGHT / 2, 5, shrink);
+        render_line(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT, 5, shrink);
         EndDrawing();
     }
 
