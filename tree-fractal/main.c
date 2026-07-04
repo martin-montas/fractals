31b4c67415ee47123e04d8e19b745de7d083bca3
martin-montas <martinmontas1@gmail.com>
2026-07-03 21:33:45 -0400

    COMMIT: Will make the code better tomorrow

--- tree-fractal/main.c
+++ tree-fractal/main.c
@@ -4,15 +4,11 @@
 #define WIDTH  1000
 #define HEIGHT 1000
 
-Vector2 draw_rotate_red(Vector2 start, Vector2 end, float angle) {
-    // finds direction
+Vector2 draw_rotate_red(Vector2 start, Vector2 end, float angle, float scale) {
     Vector2 localDirection = Vector2Subtract(start, end);
+    float   distance       = Vector2Distance(end, start);
+    Vector2 scaledLocal    = Vector2Scale(localDirection, scale);
 
-    float distance = Vector2Distance(end, start);
-    // makes it smaller
-    Vector2 scaledLocal = Vector2Scale(localDirection, 0.8f);
-
-    // STEP D: Convert degrees to radians and rotate the vector
     Vector2 rotatedScaledLocal = Vector2Rotate(scaledLocal, angle);
 
     Vector2 finalStart = Vector2Add(end, rotatedScaledLocal);
@@ -21,38 +17,30 @@ Vector2 draw_rotate_red(Vector2 start, Vector2 end, float angle) {
     return finalStart;
 }
 
-Vector2 draw_rotate_blue(Vector2 start, Vector2 end, float angle) {
-    // finds direction
-    Vector2 localDirection = Vector2Subtract(start, end);
-
-    float distance = Vector2Distance(end, start);
-    // makes it smaller
-    Vector2 scaledLocal = Vector2Scale(localDirection, 0.8f);
-
-    // STEP D: Convert degrees to radians and rotate the vector
+Vector2 draw_rotate_blue(Vector2 start, Vector2 end, float angle, float scale) {
+    Vector2 localDirection     = Vector2Subtract(start, end);
+    float   distance           = Vector2Distance(end, start);
+    Vector2 scaledLocal        = Vector2Scale(localDirection, scale);
     Vector2 rotatedScaledLocal = Vector2Rotate(scaledLocal, angle);
 
     Vector2 finalStart = Vector2Add(end, rotatedScaledLocal);
-
     DrawLineV(finalStart, end, BLUE);
     return finalStart;
 }
-float rotate_right     = 45.0f * DEG2RAD;
-float rotate_left      = 315.0f * DEG2RAD;
-float shrinkPercentage = 0.70f;
+float rotate_right = 55.0f * DEG2RAD;
+float rotate_left  = 305.0f * DEG2RAD;
+float shrink       = 0.70f;
 
-void render_line(int x1, int y1, int depth) {
+void render_line(int x1, int y1, int depth, float shrink) {
     if (depth == 0) {
         return;
     }
     Vector2 line_end   = {x1, y1};
     Vector2 line_start = {x1, y1 / 2};
-
-    // void draw_rotate_blue(Vector2 start, end, float angle, float shrink);
-    Vector2 left  = draw_rotate_blue(line_start, line_end, rotate_left);
-    Vector2 right = draw_rotate_red(line_start, line_end, rotate_right);
-
-    render_line(left.x, left.y, depth - 1);
+    Vector2 left       = draw_rotate_blue(line_start, line_end, rotate_left - 30.0f, shrink);
+    Vector2 right      = draw_rotate_red(line_start, line_end, rotate_right + 30.0f, shrink);
+    render_line(left.x, left.y, depth - 1, shrink * 0.5f);
+    render_line(right.x, right.y, depth - 1, shrink * 0.5f);
 }
 
 int main() {
@@ -64,7 +52,7 @@ int main() {
         ClearBackground(BLACK);
 
         DrawLine(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT, BLUE);
-        render_line(WIDTH / 2, HEIGHT / 2, 3);
+        render_line(WIDTH / 2, HEIGHT / 2, 5, shrink);
         EndDrawing();
     }
 
